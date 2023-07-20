cfd1e43e046510c38ace7c3c8070ba324aae0496
lucaoskaique <lucas.kaique175@hotmail.com>
2023-07-19 21:09:45 -0300

    feat: added doent and fixed code

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
 const fs = require('fs');
+
 const path = require('path');
 const ffmpeg = require("fluent-ffmpeg");
-ffmpeg.setFfmpegPath("C:/PATH_programs/ffmpeg.exe");
 
 const videosDir = './videos';
 const audiosDir = './audios';
+
+const ffmpegPath = process.env.FFMPEG_PATH || "C:/PATH_programs/ffmpeg.exe";
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 if (!fs.existsSync(audiosDir)) {
   fs.mkdirSync(audiosDir);
 }
 
+/* Para fazer a conversão de um único arquivo */
 // ffmpeg("./src/file1.mp4")
 //   .outputOptions("-vn", "-ab", "128k", "-ar", "44100")
 //   .toFormat("mp3")
@@ -17,16 +22,24 @@ if (!fs.existsSync(audiosDir)) {
 //   .on("error", console.error)
 //   .on("end", () => console.log("Conversion finished."));
 
-fs.readdirSync(videosDir).forEach(file => {
-  if (file.endsWith('.mp4')) {
-    const inputPath = path.join(videosDir, file);
-    const outputPath = path.join(audiosDir, `${path.basename(file, '.mp4')}.mp3`);
-
-    ffmpeg(inputPath)
-      .outputOptions('-vn', '-ab', '128k', '-ar', '44100')
-      .toFormat('mp3')
-      .save(outputPath)
-      .on('error', console.error)
-      .on('end', () => console.log(`Converted ${file}`));
+/* Para fazer a conversão de todos os arquivos de uma pasta */
+fs.readdir(videosDir, (err, files) => {
+  if (err) {
+    return console.error(`Error reading directory: ${err}`);
   }
+
+  files.forEach(file => {
+    // Check for other video formats
+    if (file.endsWith('.mp4') || file.endsWith('.avi') || file.endsWith('.mov')) {
+      const inputPath = path.join(videosDir, file);
+      const outputPath = path.join(audiosDir, `${path.basename(file, path.extname(file))}.mp3`);
+
+      ffmpeg(inputPath)
+        .outputOptions('-vn', '-ab', '128k', '-ar', '44100')
+        .toFormat('mp3')
+        .save(outputPath)
+        .on('error', (err) => console.error(`Error converting file: ${err}`))
+        .on('end', () => console.log(`Converted ${file}`));
+    }
+  });
 });
